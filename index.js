b7ed2413ecfce06eb8571b21d86c739423884124
REZ-OAN <ahmedabir.rez@gmail.com>
2024-04-06 23:16:13 +0600

    modified options for createProxyMiddleware for logging

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+
 function logger(req, res, next) {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
@@ -10,28 +11,30 @@ function logger(req, res, next) {
 
 app.use(logger);
 
-app.use(
-  '/:api',
-  (req, res, next) => {
-    // Extracting the :api parameter from the request URL
-    const api = req.params.api;
-    // Constructing the dynamic target URL
-    const target = `${api}.${api}.svc.cluster.local`;
-    // Logging the constructed target URL
-    console.log(`[${new Date().toISOString()}] Target URL: ${target}`);
-    // Updating the target option in the proxy middleware
-    req.proxyTarget = target;
-    next();
-  },
+app.use('/:api',
   createProxyMiddleware({
-    target: function(req) { 
-      // Accessing the dynamically constructed target URL from the request object
-      return req.proxyTarget;
-    },
+    target:"http://localhost:6060",
     ws: true,
-    logLevel: 'debug',
-  }),
-);
+    // for built  in logging of HPM it will show up on our console
+    logger:console,
+    on:{
+      proxyReq: (proxyReq, req, res) => {
+        console.log(`[Proxy Req] ${req.method} ${req.url} => ${proxyReq._headers.host}`);
+      },
+      proxyRes: (proxyRes, req, res) => {
+        console.log(`[Proxy Res] ${req.method} ${req.url} => ${proxyRes.socket._host}`);
+      },
+      // for web socket Requests
+      proxyReqWs:(proxyReq,req,socket,options, head)=>{
+        console.log(`[Proxy WS] ${req.method} ${req.url} => ${proxyReq._headers.host}`);
+      },
+      // for errors
+      error:(err,req,res,target)=>{
+        console.log(`[Proxy Err] ${err}`);
+      }
+    }
+  })
+)
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
